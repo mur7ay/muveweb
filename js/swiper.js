f430b6324f64ed839d09211004e79b79dc81818f
Evan Slaton <evanslaton@gmail.com>
2017-12-01 13:40:51 -0500

    Services slider now resizes as the window does

--- js/swiper.js
+++ js/swiper.js
@@ -1,23 +1,25 @@
 var slideCount;
 
-if ($(window).width() <= 450) {
-    slideCount = 1;
-} else if ($(window).width() >= 650) {
-    slideCount = 3;
-} else {
-    slideCount = 2;
-}
-
-var swiper = new Swiper(".swiper-container", {
-    slidesPerView: slideCount,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev"
+window.onresize = function(event) {
+    if ($(window).width() <= 450) {
+        slideCount = 1;
+    } else if ($(window).width() >= 650) {
+        slideCount = 3;
+    } else {
+        slideCount = 2;
     }
-});
+
+    var swiper = new Swiper(".swiper-container", {
+        slidesPerView: slideCount,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev"
+        }
+    });
+}
