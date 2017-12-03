52e352f894d261560f0131cf8b539e5a30fe9fa8
Evan Slaton <evanslaton@gmail.com>
2017-12-03 03:24:41 -0500

    Fixed title color problems on index2, about and blog, and fixed navbar image issue

--- js/swiper.js
+++ js/swiper.js
@@ -1,30 +1,18 @@
 var slideCount;
 
-/*function adjustSlideCount() {
-    if ($(window).width() <= 570) {
-        slideCount = 1;
-    } else if ($(window).width() >= 1000) {
-        slideCount = 3;
-    } else {
-        slideCount = 2;
-    }
-};
-
-adjustSlideCount();*/
-
 var swiper = new Swiper(".swiper-container", {
     slidesPerView: 3,
     spaceBetween: 30,
     breakpoints: {
-        // when window width is <= 320px
+        // when window width is <= 570px
         570: {
           slidesPerView: 1
         },
-        // when window width is <= 480px
-        1000: {
+        // when window width is <= 992px
+        992: {
           slidesPerView: 2
         },
-        // when window width is <= 640px
+        // when window width is <= 5000px
         5000: {
           slidesPerView: 3
         }
@@ -39,7 +27,6 @@ var swiper = new Swiper(".swiper-container", {
         prevEl: ".swiper-button-prev"
     }
 });
-
 
 window.onresize = function() {
     mySwiper.update();
