51d6afde0cde6c6a660fdccf09fb68caf290c73d
Evan Slaton <evanslaton@gmail.com>
2017-12-03 03:12:14 -0500

    Completely fixed the swiper on index2.html

--- js/swiper.js
+++ js/swiper.js
@@ -1,6 +1,6 @@
 var slideCount;
 
-function adjustSlideCount() {
+/*function adjustSlideCount() {
     if ($(window).width() <= 570) {
         slideCount = 1;
     } else if ($(window).width() >= 1000) {
@@ -10,11 +10,25 @@ function adjustSlideCount() {
     }
 };
 
-adjustSlideCount();
+adjustSlideCount();*/
 
 var swiper = new Swiper(".swiper-container", {
-    slidesPerView: slideCount,
+    slidesPerView: 3,
     spaceBetween: 30,
+    breakpoints: {
+        // when window width is <= 320px
+        570: {
+          slidesPerView: 1
+        },
+        // when window width is <= 480px
+        1000: {
+          slidesPerView: 2
+        },
+        // when window width is <= 640px
+        5000: {
+          slidesPerView: 3
+        }
+      },
     loop: true,
     pagination: {
         el: ".swiper-pagination",
@@ -26,20 +40,7 @@ var swiper = new Swiper(".swiper-container", {
     }
 });
 
-window.onresize = function() {
-    adjustSlideCount();
 
-    var swiper = new Swiper(".swiper-container", {
-        slidesPerView: slideCount,
-        spaceBetween: 30,
-        loop: true,
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true
-        },
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev"
-        }
-    }); 
+window.onresize = function() {
+    mySwiper.update();
 };
