5929e0de00ab709598647ac8ab24d745dccb5c3a
Evan Slaton <evanslaton@gmail.com>
2017-12-11 00:49:26 -0500

    Added checkmark functionality to jobSelection.html

---
+++ js/job-selection.js
@@ -0,0 +1,23 @@
+$('#job-selection1').click(function() {
+    $('#check-box1').toggleClass('checked-box');
+});
+
+$('#job-selection2').click(function() {
+    $('#check-box2').toggleClass('checked-box');
+});
+
+$('#job-selection3').click(function() {
+    $('#check-box3').toggleClass('checked-box');
+});
+
+$('#job-selection4').click(function() {
+    $('#check-box4').toggleClass('checked-box');
+});
+
+$('#job-selection5').click(function() {
+    $('#check-box5').toggleClass('checked-box');
+});
+
+$('#job-selection6').click(function() {
+    $('#check-box6').toggleClass('checked-box');
+});
