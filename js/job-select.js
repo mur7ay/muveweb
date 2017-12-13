46086df759127565e6278eab0458e722aa050c2c
Evan Slaton <evanslaton@gmail.com>
2017-12-13 09:56:18 -0500

    Added comments to job-select.js

--- js/job-select.js
+++ js/job-select.js
@@ -1,17 +1,25 @@
 var formArray = [];
 
+
+//Assigns all checkboxes to checkboxArr
 var checkboxArr = $('.checkbox-js');
-console.log(checkboxArr);
 
+
+//On-click event listener for next-btn
 $('.next-btn').click(function() {
+	formArray = [];
 
+
+	//Loops through the checkboxes and determines which are checked
 	for (i = 0; i < checkboxArr.length; i++) {
 		if ($(checkboxArr[i]).is(":checked")) {
 
-			//Adds forms to formArray
+
+			//Determines which forms needs to be displayed
 			var forms = [];
 			forms.push($('.' + i));
 
+			//Pushes forms to formArray without adding duplicates
 			for (j = 0; j < forms[0].length; j++) {
 				if ($.inArray(forms[0][j], formArray) === -1) {
 					formArray.push(forms[0][j]);
@@ -24,4 +32,3 @@ $('.next-btn').click(function() {
 	console.log(formArray);
 });
 
-// $.inArray(forms[0][j], formArray) === -1
