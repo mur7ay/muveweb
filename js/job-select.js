1ae1455f3c046840f0c71cdcb5404040931c41a7
Evan Slaton <evanslaton@gmail.com>
2017-12-14 08:43:16 -0500

    Added code for 'next' and 'back' buttons on test2.html

--- js/job-select.js
+++ js/job-select.js
@@ -1,8 +1,11 @@
 var formArray = [];
 var radioBtnForms = $('.initial-hidden');
+var counter = 0;
 
-//On-click event listener for next-btn
+
+//Click event listener for #first-next-btn
 $('#first-next-btn').click(function() {
+	formArray = [];
 
 	//Assigns all checkboxes to checkboxArr
 	var checkboxArr = $('.checkbox-js');
@@ -25,15 +28,36 @@ $('#first-next-btn').click(function() {
 			}
 		}
 
-		console.log('formArray length: ' + formArray.length);
-		console.log(formArray);
+		// console.log('formArray length: ' + formArray.length);
+		// console.log(formArray);
 
 		// console.log('radioBtnForms length: ' + radioBtnForms.length);
 		// console.log(radioBtnForms);
 
+	//Checks if at least one box is checked
 	if (formArray.length > 0) {
 		$('#jobSelect').addClass('initial-hidden');
-		$(formArray[0]).removeClass('initial-hidden');
+		$(formArray[counter]).removeClass('initial-hidden');
 	}
 });
 
+
+//Click event listener for .next-btn
+$('.next-btn').click(function() {
+	$(formArray[counter]).addClass('initial-hidden');
+	counter++;
+	$(formArray[counter]).removeClass('initial-hidden');
+});
+
+
+//Click event listener for .back-btn
+$('.back-btn').click(function() {
+	if (counter === 0) {
+		$('#jobSelect').removeClass('initial-hidden');
+		$(formArray[counter]).addClass('initial-hidden');
+	} else {
+		$(formArray[counter]).addClass('initial-hidden');
+		counter--;
+		$(formArray[counter]).removeClass('initial-hidden');	
+	}
+});
