952ab7065a72b380c0d55ae79d03cea8642cd0aa
Evan Slaton <evanslaton@gmail.com>
2017-12-14 06:51:55 -0500

    Added 'next' button functionality to checkbox question

--- js/job-select.js
+++ js/job-select.js
@@ -1,34 +1,39 @@
 var formArray = [];
-
-
-//Assigns all checkboxes to checkboxArr
-var checkboxArr = $('.checkbox-js');
-
+var radioBtnForms = $('.initial-hidden');
 
 //On-click event listener for next-btn
-$('.next-btn').click(function() {
-	formArray = [];
+$('#first-next-btn').click(function() {
+
+	//Assigns all checkboxes to checkboxArr
+	var checkboxArr = $('.checkbox-js');
+
+		//Loops through the checkboxes and determines which are checked
+		for (i = 0; i < checkboxArr.length; i++) {
+			if ($(checkboxArr[i]).is(":checked")) {
 
 
-	//Loops through the checkboxes and determines which are checked
-	for (i = 0; i < checkboxArr.length; i++) {
-		if ($(checkboxArr[i]).is(":checked")) {
+				//Determines which forms needs to be displayed
+				var forms = [];
+				forms.push($('.' + i));
 
-
-			//Determines which forms needs to be displayed
-			var forms = [];
-			forms.push($('.' + i));
-
-			//Pushes forms to formArray without adding duplicates
-			for (j = 0; j < forms[0].length; j++) {
-				if ($.inArray(forms[0][j], formArray) === -1) {
-					formArray.push(forms[0][j]);
+				//Pushes forms to formArray without adding duplicates
+				for (j = 0; j < forms[0].length; j++) {
+					if ($.inArray(forms[0][j], formArray) === -1) {
+						formArray.push(forms[0][j]);
+					}
 				}
 			}
 		}
-	}
 
-	console.log('formArray length: ' + formArray.length);
-	console.log(formArray);
+		console.log('formArray length: ' + formArray.length);
+		console.log(formArray);
+
+		// console.log('radioBtnForms length: ' + radioBtnForms.length);
+		// console.log(radioBtnForms);
+
+	if (formArray.length > 0) {
+		$('#jobSelect').addClass('initial-hidden');
+		$(formArray[0]).removeClass('initial-hidden');
+	}
 });
 
