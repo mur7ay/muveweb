6df2e4215e10d8a87e51c04a67183d9979219988
Evan Slaton <evanslaton@gmail.com>
2017-12-18 05:29:59 -0500

    Added from validation to individual forms on jobSelect.html

--- js/job-select.js
+++ js/job-select.js
@@ -7,8 +7,10 @@ var counter = 0;
 $('#first-next-btn').click(function() {
 	formArray = [];
 
+
 	//Assigns all checkboxes to checkboxArr
 	var checkboxArr = $('.checkbox-js');
+
 
 		//Loops through the checkboxes and determines which are checked
 		for (i = 0; i < checkboxArr.length; i++) {
@@ -19,6 +21,7 @@ $('#first-next-btn').click(function() {
 				var forms = [];
 				forms.push($('.' + i));
 
+
 				//Pushes forms to formArray without adding duplicates
 				for (j = 0; j < forms[0].length; j++) {
 					if ($.inArray(forms[0][j], formArray) === -1) {
@@ -28,11 +31,6 @@ $('#first-next-btn').click(function() {
 			}
 		}
 
-		// console.log('formArray length: ' + formArray.length);
-		// console.log(formArray);
-
-		// console.log('radioBtnForms length: ' + radioBtnForms.length);
-		// console.log(radioBtnForms);
 
 	//Checks if at least one box is checked
 	if (formArray.length > 0) {
@@ -44,10 +42,18 @@ $('#first-next-btn').click(function() {
 
 //Click event listener for .next-btn
 $('.next-btn').click(function() {
-	$(formArray[counter]).addClass('initial-hidden');
-	counter++;
-	$(formArray[counter]).removeClass('initial-hidden');
-	return false;
+
+	//Form validation
+	var currentForm = $(this).parent().parent();
+	var currentSelectedRB = currentForm.find(':radio').is(":checked");
+
+	if (!currentSelectedRB) {
+		alert("Please select an option")
+	} else {
+		$(formArray[counter]).addClass('initial-hidden');
+		counter++;
+		$(formArray[counter]).removeClass('initial-hidden');
+	}
 });
 
 
@@ -56,11 +62,9 @@ $('.back-btn').click(function() {
 	if (counter === 0) {
 		$('#jobSelect').removeClass('initial-hidden');
 		$(formArray[counter]).addClass('initial-hidden');
-		return false;
 	} else {
 		$(formArray[counter]).addClass('initial-hidden');
 		counter--;
 		$(formArray[counter]).removeClass('initial-hidden');
-		return false;	
 	}
 });
