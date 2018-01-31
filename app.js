8ce691c4056a07a56a63a61fa00de4606a3e1dfd
mur7ay <mur7ay@me.com>
2018-01-30 19:52:10 -0500

    Added Heroku and Stripe

--- app.js
+++ app.js
@@ -9,24 +9,36 @@ const port = process.env.PORT || 13441;
 // Set Static Folder
 app.use(express.static('public'));
 
-// Index route
+// Index route - working with Heroku but not Stripe
 // app.get('/', (req, res) => {
-//   // res.send('public/test.html', {root: __dirname});
 //   res.sendFile('public/test.html', {root: __dirname});
 // });
-//
-// app.get('/about', (req, res) => {
-//   res.sendFile('public/about.html', {root: __dirname});
-// });
 
+// Payment route
 app.get('/', function (req, res) {
   res.sendFile( __dirname + '/public/test.html' );
 });
+
+// // About page link
+// app.get('/about', (req, res) => {
+//   res.sendFile('public/about.html', {root: __dirname});
+// });
+//
+// // About page link
+// app.get('/blog', (req, res) => {
+//   res.sendFile('public/blog.html', {root: __dirname});
+// });
+//
+// // About page link
+// app.get('/moverInvite', (req, res) => {
+//   res.sendFile('public/moverInvite.html', {root: __dirname});
+// });
 
 // charge route
 app.post('/charge', (req, res) => {
   // const amount = 2500;
   const amount = req.body.totalAmount;
+  const email = req.body.
 
   stripe.customers.create({
     email: "random-email@gmail.com",
@@ -47,3 +59,9 @@ app.listen(port, () => {
 });
 
 //kade
+
+// add more app.gets to link to other pages
+// Change hosting to Heroku
+// Have payment confirmation link to confirmation page
+// Have one button submission on payment page
+// grab email on payment page
