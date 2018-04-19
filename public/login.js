36716a89b0b7d3b57120d544b63e56728d60a7bc
mur7ay <mur7ay@me.com>
2018-04-18 23:14:40 -0400

    added provider login

---
+++ public/login.js
@@ -0,0 +1,35 @@
+var object = document.getElementById('button');
+// var logout = document.getElementById('logoutBtn');
+
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    document.getElementById("user_div").style.display = "block";
+    document.getElementById("login_div").style.display = "none";
+  } else {
+    // No user is signed in.
+    document.getElementById("user_div").style.display = "none";
+    document.getElementById("login_div").style.display = "block";
+  }
+});
+
+
+object.onclick = function() {
+
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+    // ...
+  });
+}
+
+function logout() {
+  firebase.auth().signOut();
+}
