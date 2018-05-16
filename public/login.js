14b6e0b40a9f0e9c5317da725b8438a9583816a7
mur7ay <mur7ay@me.com>
2018-05-16 08:57:06 -0400

    fixed error with page rerouting and refreshing

--- public/login.js
+++ public/login.js
@@ -1,27 +1,13 @@
 var object = document.getElementById('button');
 // var logout = document.getElementById('logoutBtn');
 
-// var pushedRef = firebase.database().ref('/moving-requests').push({ email: email });
-// console.log(pushedRef.key);
-
 
 //Handle Account Status
-firebase.auth().onAuthStateChanged(user => {
-  if(user) {
-    window.location = 'contractorDashboard.html'; //After successful login, user will be redirected to home.html
-  }
-});
-
-
-// firebase.auth().onAuthStateChanged(function(user) {
-//   if (user) {
-//     // User is signed in.
-//     document.getElementById("user_div").style.display = "block";
-//     document.getElementById("login_div").style.display = "none";
+// firebase.auth().onAuthStateChanged(user => {
+//   if(user) {
+//     window.location = 'contractorDashboard.html'; //After successful login, user will be redirected to home.html
 //   } else {
-//     // No user is signed in.
-//     document.getElementById("user_div").style.display = "none";
-//     document.getElementById("login_div").style.display = "block";
+//     break;
 //   }
 // });
 
@@ -31,14 +17,12 @@ object.onclick = function() {
   var email = document.getElementById('email').value;
   var password = document.getElementById('password').value;
 
-  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-
-    window.alert("Error : " + errorMessage);
-    // ...
-  });
+  firebase.auth().signInWithEmailAndPassword(email, password).then(user => {
+        // Sign in success
+        window.location = 'contractorDashboard.html';
+    }).catch(error => {
+        console.error(error);
+    })
 }
 
 function forgotPassword() {
@@ -53,39 +37,3 @@ function logout() {
     // An error happened.
   });
 }
-
-
-// firebase.auth().onAuthStateChanged(user => {
-//   if(user) {
-//     window.location = 'contractorDashboard.html'; //After successful login, user will be redirected to home.html
-//   }
-// });
-
-// let jobsArray = [];
-// let i = 0;
-//
-// var element;
-// var ref = firebase.database().ref('moving-requests');
-// ref.on('value', function(snapshot) {
-//   snapshot.forEach(function(child) {
-//     var datas = child.val();
-//     var time = child.val().Scheduled_Time;
-//     var name = child.val().Name;
-//     var timeDriven = child.val().Time_Driven;
-//     var date = child.val().Scheduled_Date;
-//     var earnings = child.val().Estimated_Cost;
-//     date = date.replace('.', '/');
-//     console.log(name);
-//
-//     i++;
-//     $('<div>', {id:'available' + i, class:"avail-accept-jobs-div margin-bottom-twentypx"}).appendTo('#availableJobs');
-//     $('#available' + i).append('<div class="delete-job delete-avail-job"><p class="text-center font-weight-bold"><b>X</b></p></div>');
-//     $('#available' + i).append('<p>Name: ' + name + '</p>');
-//     $('#available' + i).append('<p>Move Date: ' + date + '</p>');
-//     $('#available' + i).append('<p>Move Time: ' + time + '</p>');
-//     $('#available' + i).append('<p>Drive Time: ' + timeDriven + '</p>');
-//     $('#available' + i).append('<p>Potential Earnings: $' + earnings + '</p>');
-//     $('#available' + i).append('<div class="accept-job-button text-center"><p>Accept</p></div>');
-//
-//   });
-// });
