17f6a7855e8e7d18381c0ddb2919c9f475edb468
mur7ay <mur7ay@me.com>
2018-05-17 20:08:34 -0400

    Added the ability to allow the independent contractor to upload their ID upon signup

--- public/js/contractorInvite.js
+++ public/js/contractorInvite.js
@@ -7,14 +7,29 @@ var license_image = document.getElementById('licensePhoto');
 var name, phone, email, city, age, license;
 
 
-
 document.getElementById("contractorSubmit").onclick = function() {
   name = contractors_name.value;
   phone = contractors_phone.value;
   email = contractors_email.value;
   city = contractors_city.value;
   age = age_verification.value;
-  // license = license_image.value;
+  license = license_image.files[0];
+
+  var ref = firebase.storage().ref();
+  // var file = document.querySelector('#photo').files[0]
+  var name = (+new Date()) + '-' + license.name;
+  var metadata = {
+    contentType: license.type
+  };
+  const task = ref.child(name).put(license, metadata);
+  task.then((snapshot) => {
+    const url = snapshot.downloadURL;
+    console.log(url);
+    // document.querySelector('#someImageTagID').src = url;
+  }).catch((error) => {
+    console.error(error);
+  });
+
 
   var firebaseRef = firebase.database().ref();
   firebaseRef.child("contractorInvite").push({
@@ -23,7 +38,7 @@ document.getElementById("contractorSubmit").onclick = function() {
     Email: email,
     City: city,
     Age: age,
-    // License: license
+    License: license
   }).then(function() {
     window.location.href = 'inviteConfirm.html';
   });
