a50b7edd40852219d235277f09a290856e0077c6
mur7ay <mur7ay@me.com>
2018-07-02 00:37:02 -0400

    fixed re-routing issue to ajax request

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ app.post('/charge', (req, res) => {
     currency:'USD',
     customer:customer.id
   })})
-  .then(charge => res.redirect('public/confirmationPage.html', {root: __dirname}));
+  // .then(charge => res.redirect('/index.html'));
 });
 
 
